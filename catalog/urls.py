6f48824a3d9ac82a6c47bc677326d38376a94f53
saske <saskedir@mail.ru>
2023-12-13 21:09:08 +0300

    work version

---
+++ catalog/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from catalog.views import index, contacts
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),  # URL для главной страницы
+    path('contacts/', views.contacts, name='contacts'),  # URL для страницы контактов
+]
---
+++ catalog/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'catalog/index.html')
+
+
+def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        print(f'{name}, {phone}, {message}')
+        # Делайте необходимые операции с данными из формы здесь (например, сохранение в базу данных)
+
+    return render(request, 'catalog/contacts.html')
